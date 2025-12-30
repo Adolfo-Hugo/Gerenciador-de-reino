1decd0df0d7bb13132641ce8e42ac9239dcbffe8
Adolfo-Hugo <adolfohugosilvadev@gmail.com>
2025-12-30 15:09:24 -0300

    Set base path for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Gerenciador-de-reino/',
   define: {
     // Garante que o process.env seja acessível no cliente, 
     // priorizando variáveis do sistema injetadas pelo Netlify
